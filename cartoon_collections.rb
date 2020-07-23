971a27e0423ccb724cae54434ca8e95337f89395
MARIUS MOLDOVAN <biris_21@yahoo.com>
2020-07-23 02:15:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,6 +8,12 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
+  array.each_with_index do |element|
+    puts " #{element}"
+  end
+
+
+  
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
