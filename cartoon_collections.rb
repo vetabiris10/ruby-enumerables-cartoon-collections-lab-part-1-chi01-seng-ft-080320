04310c5a7308d5c2c3fa2fb87246df92b255dc36
MARIUS MOLDOVAN <biris_21@yahoo.com>
2020-07-23 02:10:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,7 @@
 def greet_characters(array)
+    array.each do |element|
+      puts "Hello #{element}!"
+    end
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
